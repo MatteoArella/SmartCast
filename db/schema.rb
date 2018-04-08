ef51a0723abbe048c1eab24a666249a5bc9f56ce
ArellaMatteo <arella.1646983@studenti.uniroma1.it>
2018-04-08 19:35:30 +0200

    Fixed buggy migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180401094135) do
+ActiveRecord::Schema.define(version: 20180408154845) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180401094135) do
     t.string   "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "token"
   end
 
   add_index "identities", ["user_id"], name: "index_identities_on_user_id"
