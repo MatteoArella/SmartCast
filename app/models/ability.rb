cd054becb24ca5125f6933bd2095737f0828c19a
Simoncello14 <tordi.simone@yahoo.it>
2018-04-16 10:24:37 +0200

    Updated ability.rb file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,10 @@ class Ability
   def initialize(user)
     user ||= User.new
 
+    if user.guest?
+        cannot :manage, :any?
+    end
+    
     if user.learner?
         can :read, Podcast
         can :read, Episode
@@ -13,8 +17,10 @@ class Ability
     if user.artist?
         can :create, Podcast
         can :create, Episode
-        can :update, :delete, Podcast, :user_id => user.id
-        can :update, :delete, Episode, :user_id => user.id
+        can :update, Podcast, :user_id => user.id
+        can :delete, Podcast, :user_id => user.id
+        can :update, Episode, :user_id => user.id
+        can :delete, Episode, :user_id => user.id
     end
 
     if user.admin?
