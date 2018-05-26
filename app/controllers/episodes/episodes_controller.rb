c219a53158e3ce175fe2106751db2ca42db08063
ArellaMatteo <arella.1646983@studenti.uniroma1.it>
2018-05-26 18:32:27 +0200

    Modified episode show page banner

--- app/controllers/episodes/episodes_controller.rb
+++ app/controllers/episodes/episodes_controller.rb
@@ -1,8 +1,13 @@
 class Episodes::EpisodesController < ApplicationController
 
 	before_action :find_podcast # initialize @podcast variable
+	before_action :find_artist  # initialize @artist variable
 
 	def find_podcast
 		@podcast = Podcast.find(params[:podcast_id])
+	end
+
+	def find_artist
+		@artist = @podcast.artist
 	end
 end
