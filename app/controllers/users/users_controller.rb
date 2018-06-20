1eaa12c36db1a2607814f70df7e00b84520d3dec
Simoncello14 <tordi.simone@yahoo.it>
2018-06-20 18:12:42 +0200

    destroy user

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,5 +1,8 @@
 class Users::UsersController < ApplicationController
-
+  def index
+    @users = User.all.paginate(:page => params[:page])
+    render template: 'users/index'
+  end
   def show
     @user = User.find(params[:id])
     render template: 'users/show'
