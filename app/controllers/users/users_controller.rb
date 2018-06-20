3e8e6b7d035de6b8935299ef1a5f9a5ca4947f75
Simoncello14 <tordi.simone@yahoo.it>
2018-06-20 15:15:38 +0200

    Implementatuion of delete action of users

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -4,4 +4,14 @@ class Users::UsersController < ApplicationController
     @user = User.find(params[:id])
     render template: 'users/show'
   end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    if @user.destroy
+    	redirect_to root_path, notice: "User deleted"
+    end
+  end
+
 end
